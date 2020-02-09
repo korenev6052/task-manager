6274942c4b06172dcfd8c8445569c4a21044ab45
Aleksandr Korenev <korenev6052@gmail.com>
2020-02-09 21:36:01 +0300

    tasks service

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { SystemRoutingModule } from './system-routing.module';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TasksService } from './shared/services/tasks.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,9 @@ import { TaskListComponent } from './task-list/task-list.component';
     CommonModule,
     SystemRoutingModule,
     SharedModule
+  ],
+  providers: [
+    TasksService
   ]
 })
 export class SystemModule { }
